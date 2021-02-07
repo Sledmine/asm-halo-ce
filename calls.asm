ace9cadb4f77253a09f35cd58cdc7466be8b26d1
Sled <angelherxd@hotmail.com>
2021-02-07 16:38:36 -0700

    calls to render and stuff
    This are some of the calls I found while doing some research on OllyDbg

---
+++ calls.asm
@@ -0,0 +1,18 @@
+0050F6BB     E8 00020000    CALL haloce.0050F8C0 - Master Render call
+0050F998     E8 037FF8FF    CALL haloce.004978A0 - ???
+0050F99D     E8 DE520000    CALL haloce.00514C80 - ???
+0050F9A2     E8 194B0000    CALL haloce.005144C0 - Sky render call
+0050F9A7     E8 4458F8FF    CALL haloce.004951F0 - First person render call
+0050F9AC     E8 4F4CFEFF    CALL haloce.004F4600 - ???
+0050F9B1     E8 8A280000    CALL haloce.00512240 - Object render call (?)
+0050F9B6     E8 75620400    CALL haloce.00555C30 - Bsp poly render call (???)
+0050F9BB     E8 70630400    CALL haloce.00555D30 - Bsp render call
+0050F9C0     E8 6B7A0000    CALL haloce.00517430 - Lens flare render call (?)
+0050F9F3     E8 882A0000    CALL haloce.00512480 - Shadow render call
+0050F9F8     E8 9356FEFF    CALL haloce.004F5090 - ???
+0051236A     EB 18          JMP SHORT haloce.00512384 -- Third person render conditional
+0050FA87     E8 54ED0000    CALL haloce.0051E7E0 - Bsp shaders render call (?)
+0050FD04     E8 E7230100    CALL haloce.005220F0 - Poly fog render call (???)
+0050FD22     E8 F9640400    CALL haloce.00556220 - Fog render call
+0050FE61     E8 8A76F8FF    CALL haloce.004974F0 - HUD render call + F1, F2 Tables (?)
+0050F797     E8 746F0000    CALL haloce.00516710 - FPS rasterizer call (?)
